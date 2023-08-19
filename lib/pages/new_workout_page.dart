e716572b32b22c7611c0d92637278514af4fc1e2
skeetztoon <1014day@gmail.com>
2023-08-19 23:11:59 +0300

    Fixed sessions list render, added on-close-dialogue for workout

--- lib/pages/new_workout_page.dart
+++ lib/pages/new_workout_page.dart
@@ -24,143 +24,175 @@ class _NewWorkoutState extends State<NewWorkout> with TickerProviderStateMixin {
   late Timer timer;
   String elapsedTime = '00:00:00';
 
+
   @override
   Widget build(BuildContext context) {
     timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
       setState(() {
         elapsedTime =
-            '${(stopwatch.elapsed.inHours).toString().padLeft(2, '0')}:'
+        '${(stopwatch.elapsed.inHours).toString().padLeft(2, '0')}:'
             '${(stopwatch.elapsed.inMinutes % 60).toString().padLeft(2, '0')}:'
             '${(stopwatch.elapsed.inSeconds % 60).toString().padLeft(2, '0')}';
       });
     });
     stopwatch.start();
 
-    return Scaffold(
-        backgroundColor: Colors.black.withOpacity(0.5),
-        body: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            height: MediaQuery.of(context).size.height * 0.95,
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-              border: Border.all(width: 10, color: const Color(0xFF3b3b3b)),
-              color: const Color(0xff282828),
-            ),
-            child: Column(
-              children: [
-                const Text(
-                  "NEW WORKOUT",
-                  style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  elapsedTime,
-                  style: const TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                  ),
-                ),
+    return WillPopScope(
+      onWillPop: () async {
 
-                ConstrainedBox(
-                  constraints: const BoxConstraints(maxHeight: 450),
-                  child: ListView.builder(
-                      reverse: true,
-                      shrinkWrap: true,
-                      controller: controller,
-                      itemCount: exerciseCounter.length,
-                      itemBuilder: (context, index) {
-                        final item = exerciseCounter[index];
-                        return Dismissible(
-                            key: Key(item.toString()),
-                            onDismissed: (direction) {
-                              setState(() {
-                                exerciseCounter.removeAt(index);
-                              });
-                            },
-                            background: Container(color: Colors.red),
-                            child: ExerciseCard(
-                              dropdownValues: dropdownValues,
-                              index: index,
-                            ));
-                      }),
-                ),
-
-                const SizedBox(height: 20,),
-
-                FloatingActionButton.extended(
-                  //                       КНОПКА
-                  label: const Text(
-                    'ADD',
+        final result = await showDialog(
+          builder: (context) => AlertDialog(
+            backgroundColor: const Color(0xff282828),
+            title: const Text('Are you sure?', style: TextStyle(color: Colors.white)),
+            content: const Text('All progress will be lost', style: TextStyle(color: Colors.white)),
+            actions: <Widget>[
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.pink),
+                child: const Text('No'),
+                onPressed: () {
+                  Navigator.pop(context, false);
+                },
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.pink),
+                child: const Text('Yes'),
+                onPressed: () {
+                  Navigator.pop(context, true);
+                },
+              ),
+            ],
+          ), context: context,
+        );
+        return result;
+      },
+      child: Scaffold(
+          backgroundColor: Colors.black.withOpacity(0.5),
+          body: Center(
+            child: Container(
+              margin: const EdgeInsets.only(top: 20.0),
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.9,
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                border: Border.all(width: 10, color: const Color(0xFF3b3b3b)),
+                color: const Color(0xff282828),
+              ),
+              child: Column(
+                children: [
+                  const Text(
+                    "NEW WORKOUT",
                     style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87),
+                        fontSize: 40,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
                   ),
-                  backgroundColor: Colors.pink,
-                  icon: const Icon(
-                    Icons.add_circle_rounded,
-                    color: Colors.black87,
-                    size: 24.0,
+                  const SizedBox(
+                    height: 10,
                   ),
-                  onPressed: () {
-                    setState(() {
-                      exerciseCounter
-                          .add(exerciseCounter[exerciseCounter.length - 1] + 1);
-                      Timer(const Duration(milliseconds: 100), () {
-                        controller.jumpTo(controller.position.maxScrollExtent); // Scroll to last item
-                      });
-                      dropdownValues.add("");
-                    });
-                  },
-                ),
-
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: SwipeableButtonView(
-                      activeColor: Colors.pink,
-                      buttonText: "FINISH",
-                      buttonWidget: const Icon(
-                        Icons.arrow_forward_ios_rounded,
-                        color: Colors.grey,
-                      ),
-                      isFinished: isFinished,
-                      onWaitingProcess: () {
-                        String dateStr =
-                            "${DateTime.now().day}-${DateTime.now().month}-${DateTime.now().year}";
-
-                        timer.cancel();
-                        stopwatch.stop(); // Секундомер
-                        List<String> filteredDropdownValues = dropdownValues.where((value) => value.isNotEmpty).toList();
-
-                        CollectionReference myWorkouts =
-                            FirebaseFirestore.instance.collection('MyWorkouts');
-                        myWorkouts.add({
-                          'Date': dateStr,
-                          'Timer': elapsedTime,
-                          'Exercises': filteredDropdownValues
-                        });
-
-                        setState(() {
-                          isFinished = true;
-                        });
-                      },
-                      onFinish: () {
-                        Navigator.pop(context);
-                      },
+                  Text(
+                    elapsedTime,
+                    style: const TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
                     ),
                   ),
-                )
-              ],
+
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(maxHeight: 400),
+                    child: ListView.builder(
+                        reverse: true,
+                        shrinkWrap: true,
+                        controller: controller,
+                        itemCount: exerciseCounter.length,
+                        itemBuilder: (context, index) {
+                          final item = exerciseCounter[index];
+                          return Dismissible(
+                              key: Key(item.toString()),
+                              onDismissed: (direction) {
+                                setState(() {
+                                  exerciseCounter.removeAt(index);
+                                });
+                              },
+                              background: Container(color: Colors.red),
+                              child: ExerciseCard(
+                                dropdownValues: dropdownValues,
+                                index: index,
+                              ));
+                        }),
+                  ),
+
+                  const SizedBox(height: 20,),
+
+                  FloatingActionButton.extended(
+                    //                       КНОПКА
+                    label: const Text(
+                      'ADD',
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87),
+                    ),
+                    backgroundColor: Colors.pink,
+                    icon: const Icon(
+                      Icons.add_circle_rounded,
+                      color: Colors.black87,
+                      size: 24.0,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        exerciseCounter
+                            .add(exerciseCounter[exerciseCounter.length - 1] + 1);
+                        Timer(const Duration(milliseconds: 100), () {
+                          controller.jumpTo(controller.position.maxScrollExtent); // Scroll to last item
+                        });
+                        dropdownValues.add("");
+                      });
+                    },
+                  ),
+
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: SwipeableButtonView(
+                        activeColor: Colors.pink,
+                        buttonText: "FINISH",
+                        buttonWidget: const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Colors.grey,
+                        ),
+                        isFinished: isFinished,
+                        onWaitingProcess: () {
+                          String dateStr =
+                              "${DateTime.now().day}-${DateTime.now().month}-${DateTime.now().year}";
+
+                          timer.cancel();
+                          stopwatch.stop(); // Секундомер
+                          List<String> filteredDropdownValues = dropdownValues.where((value) => value.isNotEmpty).toList();
+
+                          CollectionReference myWorkouts =
+                          FirebaseFirestore.instance.collection('MyWorkouts');
+                          myWorkouts.add({
+                            'Date': dateStr,
+                            'Timer': elapsedTime,
+                            'Exercises': filteredDropdownValues
+                          });
+
+                          setState(() {
+                            isFinished = true;
+                          });
+                        },
+                        onFinish: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ));
+          )),
+    );
+
   }
 }
--- lib/pages/sessions_list.dart
+++ lib/pages/sessions_list.dart
@@ -13,57 +13,65 @@ class _SessionsListState extends State<SessionsList> {
   final Stream<QuerySnapshot> exercisesList =
       FirebaseFirestore.instance.collection("MyWorkouts").snapshots();
 
-  final List sessions = [
-    1,
-    2,
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff282828),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 80,
-          ),
-          const Text(
-            "YOUR SESSIONS",
-            style: TextStyle(
-                fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-            stream: exercisesList,
-            builder: (
-              BuildContext context,
-              AsyncSnapshot<QuerySnapshot> snapshot,
-            ) {
-              if (snapshot.hasError) {
-                return const Text("Something went wrong");
-              }
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Text("Loading");
-              }
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 1,
+              color: const Color(0xff282828),
+              child: const Column(
+                children: [
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Text(
+                    "YOUR SESSIONS",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection("MyWorkouts").orderBy("Date", descending: true).snapshots(),
+              builder: (
+                BuildContext context,
+                AsyncSnapshot<QuerySnapshot> snapshot,
+              ) {
+                if (snapshot.hasError) {
+                  return const Text("Something went wrong");
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Text("Loading");
+                }
 
-              final data = snapshot.requireData;
+                final data = snapshot.requireData;
 
-              return ListView.builder(
-                itemCount: data.size,
-                itemBuilder: (context, index) {
-                  return Session(
-                      date: data.docs[index]["Date"],
-                      exercises: data.docs[index]["Exercises"],
-                      timer: data.docs[index]["Timer"],
-                      index: data.docs[index].reference.id);
-                },
-              );
-            },
-          )),
-        ],
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: data.size,
+                  itemBuilder: (context, index) {
+                    return Session(
+                        date: data.docs[index]["Date"],
+                        exercises: data.docs[index]["Exercises"],
+                        timer: data.docs[index]["Timer"],
+                        index: data.docs[index].reference.id);
+                  },
+                );
+              },
+            )),
+          ],
+        ),
       ),
     );
   }
